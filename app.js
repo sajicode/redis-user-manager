f1a730df4eaeadd580390d63c1123110cd78f3ad
root <root@Adams.localdomain>
2018-12-08 14:28:01 +0100

    create searchusers page and connect redis

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const redis = require('redis');
 
+// Create redis client
+let client = redis.createClient();
+
+client.on('connect', function () {
+  console.log("Connected to Redis");
+});
 
 // Set port
 const port = 3000;
@@ -30,6 +36,24 @@ app.use(methodOverride('_method'));
 
 app.get('/', function (req, res, next) {
   res.render('searchusers');
+});
+
+// Search page
+app.post('/user/search', function (req, res, next) {
+  let id = req.body.id;
+
+  client.hgetall(id, function (err, obj) {
+    if (!obj) {
+      res.render('searchusers', {
+        error: 'User does not exist'
+      });
+    } else {
+      obj.id = id;
+      res.render('details', {
+        user: obj
+      });
+    }
+  });
 });
 
 app.listen(port, () => {
