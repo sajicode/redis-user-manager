ac25884c8633a972b414e53b3762474e4373a7c3
root <root@Adams.localdomain>
2018-12-08 15:11:11 +0100

    add routes for adding and deleting users

--- app.js
+++ app.js
@@ -56,6 +56,40 @@ app.post('/user/search', function (req, res, next) {
   });
 });
 
+// add user page
+app.get('/user/add', function (req, res, next) {
+  res.render('adduser');
+});
+
+// process adduser page
+app.post('/user/add', function (req, res, next) {
+  let id = req.body.id;
+  let first_name = req.body.first_name;
+  let last_name = req.body.last_name;
+  let email = req.body.email;
+  let phone = req.body.phone;
+
+  client.hmset(id, [
+    'first_name', first_name,
+    'last_name', last_name,
+    'email', email,
+    'phone', phone
+  ], function (err, reply) {
+    if (err) {
+      console.log(err);
+    }
+    console.log(reply);
+    res.redirect('/');
+  });
+});
+
+// delete uder
+app.delete('/user/delete/:id', function (req, res, next) {
+  client.del(req.params.id);
+  res.redirect('/');
+});
+
+
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
